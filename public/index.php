0f17f1d9c204ac72cc960ce4029bec574b7c1531
johnnerrycamarig <johnnerrycamarig@gmail.com>
2026-09-13 20:20:29 +0800

    ci: add neon database branch workflow for pull requests

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+set_time_limit(120);
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
